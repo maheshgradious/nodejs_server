2f32b54d15570bad3c4b31700497f4c170b56613
abdulmalik07-m <119485850+abdulmalik07-m@users.noreply.github.com>
2023-04-10 17:29:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '172.17.0.3',  //changed localhost to mysqlcont ip
+    host: '162.17.0.3',  //changed localhost to mysqlcont ip
     user: 'mmuser',
     password: 'Malik@03',
     database: 'doctor_appointment'
